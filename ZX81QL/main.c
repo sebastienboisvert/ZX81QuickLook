ba1ddecd8e37fb9f78465b4634ecb0e015b9759a
Sebastien Boisvert <sebastienboisvert@yahoo.com>
2020-01-02 21:10:33 -0500

    Reworked the CLI tool for more validation and output.
    Ensure file can be loaded, and outputs some of the system variables.

--- ZX81QL/main.c
+++ ZX81QL/main.c
@@ -12,26 +12,50 @@
 
 int main(int argc, const char * argv[]) {
 
-    char basic_output[114700] = "COULD NOT LOAD BASIC FILE";
-
-    struct string_buffer code_buffer;
-    code_buffer.buffer = basic_output;
-    code_buffer.buffer_length = sizeof(basic_output);
-    
-    char screen_output[800] = "NO SCREEN FOUND";
-    struct string_buffer screen_buffer;
-    screen_buffer.buffer = screen_output;
-    screen_buffer.buffer_length = sizeof(screen_output);
-    
     FILE *in = fopen(argv[1], "r");
-    
+
     if(NULL != in) {
-        get_basic_listing(code_buffer,in);
-        get_screen(screen_buffer, in);
+        
+        char basic_output[114700] = "COULD NOT LOAD BASIC FILE";
+
+        struct ZX81_Program program;
+        
+        struct string_buffer code_buffer;
+        code_buffer.buffer = basic_output;
+        code_buffer.buffer_length = sizeof(basic_output);
+        
+        char screen_output[800] = "NO SCREEN FOUND";
+        struct string_buffer screen_buffer;
+        screen_buffer.buffer = screen_output;
+        screen_buffer.buffer_length = sizeof(screen_output);
+        
+        program = checkFile(in);
+    
+        if(valid == program.status) {
+            printf("Program: %0X\n",FILE_START+program.PROGRAM);
+            printf("DFILE: %0.0X\n",FILE_START+program.D_FILE);
+            printf("VARS: %0X\n",FILE_START+program.VARS);
+            printf("ELINE: %0X\n\n",FILE_START+program.E_LINE);
+            
+            get_basic_listing(code_buffer,in);
+            get_screen(screen_buffer, in);
+
+            printf("BASIC:\n%s\nSCREEN:\n%s\n",basic_output,screen_output);
+
+        }
+        else {
+            printf("Invalid BASIC file.");
+        }
+        
         fclose(in);
     }
-    
-    printf("BASIC:\n%s\nSCREEN:\n%s\n",basic_output,screen_output);
+    else {
+        if(argv[1]) {
+            printf("Could not open file %s.\n",argv[1]);
+        } else {
+            printf("No file provided.\n");
+        }
+    }
     
     return 0;
 }
